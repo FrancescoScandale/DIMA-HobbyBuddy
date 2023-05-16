d9a78f6371ae52fc1075c9c41db3e51c6233a100
MartaRadaelli <marta.radaelli99@gmail.com>
2023-05-16 20:32:04 +0200

    Update main.dart

--- hobbybuddy/lib/main.dart
+++ hobbybuddy/lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hobbybuddy/firebase_options.dart';
 
 String logo = 'assets/logo.png';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
