1f7edd5810c8ff9584cbc6e3b17a0e49d952fdc5
FrancescoScandale <80072748+FrancescoScandale@users.noreply.github.com>
2023-07-12 01:19:04 +0200

    buggy login

--- hobbybuddy/lib/main.dart
+++ hobbybuddy/lib/main.dart
@@ -12,8 +12,9 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const ShowLoginCredentials());
+  //runApp(const ShowLoginCredentials());
   //runApp(const MartaScreen());
+  runApp(const LoginScreen());
 }
 
 class MartaScreen extends StatelessWidget {
@@ -25,8 +26,7 @@ class MartaScreen extends StatelessWidget {
     return MaterialApp(
       title: "hobbybuddy",
       home: Directionality(
-        textDirection:
-            TextDirection.ltr, // Replace with the appropriate text direction
+        textDirection: TextDirection.ltr, // Replace with the appropriate text direction
         child: Scaffold(
           backgroundColor: Colors.white,
           body: Center(
@@ -146,32 +146,146 @@ class ShowLoginCredentials extends StatelessWidget {
             child: Text(
               document['password'],
               style: Theme.of(context).textTheme.headlineSmall,
-            ), // Text
-          ), // Expanded
+            ),
+          ),
         ],
       ),
-    ); // ListTile
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "hobbybuddy",
       home: Scaffold(
         appBar: AppBar(title: Text(title)),
         body: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('credentials')
-                .snapshots(),
+            stream: FirebaseFirestore.instance.collection('credentials').snapshots(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) return const Text('Data not found :(');
               return ListView.builder(
                 itemExtent: 80.0,
                 itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) =>
-                    _buildListItem(context, snapshot.data!.docs[index]),
+                itemBuilder: (context, index) => _buildListItem(context, snapshot.data!.docs[index]),
               );
             }),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'hobbybuddy';
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  Map<String, dynamic> credentials = {};
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  void retrieveCredentials() async {
+    final snap = await FirebaseFirestore.instance.collection("credentials").get();
+    credentials = snap.docs.first.data();
+    print("credentials -> " + credentials.toString());
+    print("keys -> " + credentials.keys.toString());
+    print("passwords -> " + credentials.values.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    retrieveCredentials();
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: username,
+            // The validator receives the text that the user has entered.
+            validator: (value1) {
+              if (value1 == null || value1.isEmpty) {
+                return 'username not found';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: password,
+            // The validator receives the text that the user has entered.
+            validator: (value2) {
+              if (value2 == null || value2.isEmpty) {
+                return 'password not found';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  bool check = false;
+                  //check if credentials contained
+                  if (credentials.containsKey(username.text) && credentials.containsValue(password.text)) {
+                    if (credentials[username.text] == password.text) {
+                      check = true;
+                    }
+                    if (check) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Found!')),
+                      );
+                    }
+                  }
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  if (!check) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Data not found...")),
+                    );
+                  }
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
